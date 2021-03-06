86d7e404c865dbb378bd0903167164b391af81ee
shurintou <shurintou@gmail.com>
2021-03-07 01:07:38 +0900

    删除不必要代码

--- config/index.js
+++ config/index.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
-var f
 if(process.env.NODE_ENV === 'development'){
-    f = 'config-development.js'
+    var f = 'config-development.js'
     console.log('import config from file ' + f); 
     module.exports = require('../config/' + f);
 
 }
 else{
-    f = 'config-production.js'
+    var f = 'config-production.js'
     console.log('import config from file ' + f );
     module.exports = require('../config/' + f);
 }
