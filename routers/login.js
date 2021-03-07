b963e4266e22f3ac7a6bbb47478562c697c1c3a6
shurintou <shurintou@gamil.com>
2021-03-08 00:49:24 +0900

    加入session管理权限。加入redis储存session。配置全局路由拦截器。

--- routers/login.js
+++ routers/login.js
@@ -5,6 +5,10 @@ const loginService = require('../service/loginService')
 router.post('/login', function (req, res) {
     loginService.login(req.body)
     .then(result => {
+        /* code为200则登录成功，设置session */
+        if(result.code === 200){
+            req.session.username = req.body.username
+        }
         res.status(200).json({code: result.code,  message: result.message})
     })
     .catch(err => {
