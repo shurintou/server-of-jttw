fbdc761154a9c87696e6cc36ec51f76b15d451c5
shurintou <shurintou@gmail.com>
2021-03-10 01:55:19 +0900

    websocket通过session管理

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var session = require('./common/session').session
 var app = express();
+const http = require('http');
+const wss = require('./websocket/')
 const routers = require('./common/routers')
 const routerInterceptor = require('./common/routerInterceptor')
 
@@ -21,6 +23,22 @@ app.use(session)
 app.use('*', routerInterceptor)//拦截器
 /**********/
 
+/* websocket */
+const server = http.createServer(app);
+server.on('upgrade', function (request, socket, head) {
+  session(request, {}, () => {
+    if (request.session.username === undefined || !request.session.username) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    }
+    wss.handleUpgrade(request, socket, head, function (ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+});
+/************/
+
 /* API*(路由) */
 Object.keys(routers).forEach(key => {
     app.use(conf.APIRoot, routers[key])//给路由增加根路径
@@ -28,6 +46,7 @@ Object.keys(routers).forEach(key => {
 /*******/
 
 /* 启动服务器 */
-app.listen( conf.port, () => {} )
+// app.listen( conf.port, () => {} )
+server.listen(conf.port, () => {}) 
 /*************/
 
