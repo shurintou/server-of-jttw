fcd42c78611ad1a1e863018399377ca5b018fc57
shurintou <shurintou@gamil.com>
2021-03-12 02:36:45 +0900

    修改Login相关代码

--- services/loginService.js
+++ services/loginService.js
@@ -2,17 +2,17 @@ const models = require('../common/models')
 const store = require('../common/session').store
 
 module.exports = {
-    login: async function(data){
+    login: async function(req){
         try{
             const Account = models.account
-            var accounts = await Account.findAll({where:{username : data.username}})
+            var accounts = await Account.findAll({where:{username : req.body.username}})
             if(accounts.length === 0){
                 return Promise.resolve({code: 406, message: '用户名不存在，请重新输入'})
             }
             else{
-                if(accounts[0].password === data.password){
+                if(accounts[0].password === req.body.password){
                     /* store.all是回调函数形式的异步，所以用Promise包裹一层使得其能够同步执行 */
-                    return await storeWrapper(data, accounts[0])
+                    return await storeWrapper(req, accounts[0])
                 }
                 else{
                     return Promise.resolve({code: 406, message: '密码不正确，请重新输入'})
@@ -27,7 +27,7 @@ module.exports = {
     logout: function(req){store.destroy(req.sessionID, function(){})}
 }
 
-function storeWrapper(data, account){
+function storeWrapper(req, account){
     return new Promise((resolve, reject) => {
         /* 通过验证能够登录，但须确认有没有重复session */
         store.all(function(err, sessions){
@@ -36,13 +36,12 @@ function storeWrapper(data, account){
             }
             else{
                var times = 0
-               console.log('length', sessions.length)
                for(var i = 0; i < sessions.length; i++){
-                    if(sessions[i].username === data.username){
+                    if(sessions[i].username === req.body.username){
                         times = times + 1 
                     }
                }
-               if(times === 1){
+               if(times > 0){
                  return resolve({code: 409, message: '请勿重复登录'})
                }
                else{
