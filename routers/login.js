df5e6d0058e4e49d90e6da66e63f916b801f5182
shurintou <shurintou@gamil.com>
2021-03-12 15:13:57 +0900

    失活session定期删除

--- routers/login.js
+++ routers/login.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const loginService = require('../services/loginService')
+const sessionHandler = require('../common/session').sessionHandler
 
 router.post('/login', function (req, res) {
     loginService.login(req)
     .then(result => {
         /* code为200则登录成功，设置session */
         if(result.code === 200){
-            req.session.username = result.account.username
-            req.session.userId = result.account.id
-            req.session.nickname = result.account.nickname
-            req.session.sessionID = req.sessionID
+            sessionHandler(req, result.account)
             res.status(200).json({code: result.code, message: result.message, account: result.account})
         }
         else if(result.code === 409){
