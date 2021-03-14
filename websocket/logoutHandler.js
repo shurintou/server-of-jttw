697bac981085fa23358a17fd19da3c23c4a980d8
shurintou <shurintou@gmail.com>
2021-03-15 00:36:39 +0900

    增加玩家上下线系统提示

--- websocket/logoutHandler.js
+++ websocket/logoutHandler.js
@@ -8,7 +8,7 @@ module.exports= function(wss){
         if (err) {return console.error('error redis response - ' + err)}
         var livePlayer = []
         var removePlayer = []
-        /* 2，获取redis中失活的session */
+        /* 2，获取redis中sess:命名空间下失活的session */
         store.all( function(err, sessions){
             if (err) {return console.error('error redis response - ' + err)}
             sessions.forEach( session => {
@@ -23,11 +23,23 @@ module.exports= function(wss){
                     }
                 }
                 if(!isLive){
+                    redis.get(item, function(err, res){
+                        if (err) {return console.error('error redis response - ' + err)}
+                        wss.clients.forEach(function each(client) {
+                            if (client.readyState === WebSocket.OPEN) {
+                                client.send(JSON.stringify({type: 'system', player_loc: 0 , text: '玩家 ' + JSON.parse(res).nickname + ' 下线了'}))
+                                if(res.player_loc > 0){
+                                    client.send(JSON.stringify({type: 'system', player_loc: res.player_loc , text: '玩家 ' + JSON.parse(res).nickname + ' 退出了房间'}))
+                                }
+                            }
+                        });
+                        redis.del(item)
+                    })
+                    /* 3，把1和2的结果对比，获取playerList中应该删除的玩家 */
                     removePlayer.push(item)
-                    redis.del(item)
                 }
             })
-            /* 3，对应删除失活session的player */
+            /* 4，删除player */
             redis.srem('playerList', removePlayer, function(){
                 redis.smembers('playerList', 
                 function(err, list){
@@ -35,7 +47,7 @@ module.exports= function(wss){
                     if (list.length > 0){
                         redis.mget(list, function(err, playerList){
                             if (err) {return console.error('error redis response - ' + err)}
-                            /* 4，广播 */
+                            /* 5，广播 */
                             wss.clients.forEach(function each(client) {
                                 if (client.readyState === WebSocket.OPEN) {
                                     client.send(JSON.stringify({type: 'playerList', data: playerList}));
--- websocket/playerListHandler.js
+++ websocket/playerListHandler.js
@@ -2,6 +2,7 @@ const redis = require('../database/redis')
 const WebSocket = require('ws');
 
 module.exports = function(data ,wss, req){
+        /* 1，设置玩家最新信息，覆盖掉旧信息 */
         redis.set('player:' + req.session.userId,
         JSON.stringify({
             id: req.session.userId,
@@ -13,9 +14,18 @@ module.exports = function(data ,wss, req){
         }), 
         function(err){
             if (err) {return console.error('error redis response - ' + err)}
+            /* 2，向playerList中添加该玩家，因为是set类型所以不会重复。返回结果是1则代表新上线，否则是刷新信息 */
             redis.sadd('playerList', 'player:' + req.session.userId,
-            function(err){
+            function(err, res){
                 if (err) {return console.error('error redis response - ' + err)}
+                if (res === 1){
+                    wss.clients.forEach(function each(client) {
+                        if (client.readyState === WebSocket.OPEN && client.username !== req.session.username) {
+                            client.send(JSON.stringify({type: 'system', player_loc: 0 , text: '玩家 ' + data.nickname + ' 上线了'}))
+                        }
+                    });
+                }
+                /* 3，获取所有playerList中玩家，发送广播 */
                 redis.smembers('playerList', 
                 function(err, list){
                     if (err) {return console.error('error redis response - ' + err)}
