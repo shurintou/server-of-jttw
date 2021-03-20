94c1e8bde1925827dddc2b82bf270e3e7411a5aa
shurintou <shurintou@gamil.com>
2021-03-21 01:27:13 +0900

    修改创建房间逻辑，增加离开房间逻辑

--- websocket/gameRoomListHandler.js
+++ websocket/gameRoomListHandler.js
@@ -3,24 +3,28 @@ const WebSocket = require('ws');
 const conf = require('../config/')
 
 module.exports = function(data ,wss, ws){
-    /* id是-1则直接获取现在的游戏房间列表 */
-    if(data.id === -1){
+    /* id是0则直接获取现在的游戏房间列表 */
+    if(data.id === 0){
         redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
             if (err) {return console.error('error redis response - ' + err)}
-            if(list.length === 0){ return }
+            if(list.length === 0){ 
+                ws.send(JSON.stringify({type: 'gameRoomList', data: [] }));
+                return 
+            }
             redis.mget(list, function(err, gameRoomList){
                 if (err) {return console.error('error redis response - ' + err)}
                 ws.send(JSON.stringify({type: 'gameRoomList', data: gameRoomList}));
             })
         })
     }
-    /* id位0是新建的房间，需要分配一个id */
-    else if(data.id === 0){
+    /* id位NaN是新建的房间，需要分配一个id */
+    else if(data.id === null || data.id === NaN){
         redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
             if (err) {return console.error('error redis response - ' + err)}
             var idOfList = []
             list.forEach( item => { idOfList.push(parseInt(item.split(conf.redisCache.gameRoomPrefix)[1]))})
             idOfList.sort()
+            /* 分配的房间号 */
             var freeIndex = 0
             for(var i = 0; i < idOfList.length; i++){
                 if( idOfList[i] !== i + 1 ){
@@ -52,6 +56,66 @@ module.exports = function(data ,wss, ws){
                 })
             })
         })        
+    }
+    else if(data.id < 0){
+        /* 小于0，某玩家离开了房间 */
+        redis.get(conf.redisCache.gameRoomPrefix + (-1 * data.id), function(err, res){
+            if (err) {return console.error('error redis response - ' + err)}
+            var room = JSON.parse(res)
+            /* 过滤掉离开的玩家 */
+            var newPlayerList = room.playerList.filter( player => player.id !==  ws.userId)
+            /* 如果还有玩家在房间中则保留房间并广播 */
+            if(newPlayerList.length > 0){
+                room.playerList = newPlayerList
+                redis.set(conf.redisCache.gameRoomPrefix + (-1 * data.id), JSON.stringify(room), function(err){
+                    if (err) {return console.error('error redis response - ' + err)}
+                    redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+                        if (err) {return console.error('error redis response - ' + err)}
+                        if(list.length === 0){ 
+                            wss.clients.forEach(function each(client) {
+                                if (client.readyState === WebSocket.OPEN) {
+                                    client.send(JSON.stringify({type: 'gameRoomList', data: [] }));
+                                }
+                            })
+                            return
+                        }
+                        redis.mget(list, function(err, gameRoomList){
+                            if (err) {return console.error('error redis response - ' + err)}
+                            wss.clients.forEach(function each(client) {
+                                if (client.readyState === WebSocket.OPEN) {
+                                    client.send(JSON.stringify({type: 'gameRoomList', data: gameRoomList}));
+                                }
+                            });
+                        })
+                    })
+                })
+            }
+            /* 否则删除房间并广播 */
+            else{
+                redis.del(conf.redisCache.gameRoomPrefix + (-1 * data.id), function(err){
+                    if (err) {return console.error('error redis response - ' + err)}
+                    redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+                        if (err) {return console.error('error redis response - ' + err)}
+                        if(list.length === 0){ 
+                            wss.clients.forEach(function each(client) {
+                                if (client.readyState === WebSocket.OPEN) {
+                                    client.send(JSON.stringify({type: 'gameRoomList', data: [] }));
+                                }
+                            })
+                            return
+                        }
+                        redis.mget(list, function(err, gameRoomList){
+                            if (err) {return console.error('error redis response - ' + err)}
+                            wss.clients.forEach(function each(client) {
+                                if (client.readyState === WebSocket.OPEN) {
+                                    client.send(JSON.stringify({type: 'gameRoomList', data: gameRoomList}));
+                                }
+                            });
+                        })
+                    })
+                })
+            }
+        })
     }
     
 }
--- websocket/playerListHandler.js
+++ websocket/playerListHandler.js
@@ -3,18 +3,8 @@ const WebSocket = require('ws');
 const conf = require('../config/')
 
 module.exports = function(data ,wss, req){
-    /* 1，检查该key是否存在，不存在则是新上线，否则是刷新信息 */
-    redis.exists(conf.redisCache.playerPrefix + req.session.userId, function(err, res){
-        if (err) {return console.error('error redis response - ' + err)}
-        if(res === 0){
-            wss.clients.forEach(function each(client) {
-                if (client.readyState === WebSocket.OPEN && client.username !== req.session.username) {
-                    client.send(JSON.stringify({type: 'system', player_loc: 0 , text: '玩家 ' + data.nickname + ' 上线了'}))
-                }
-            })
-        }
-        /* 2，设置玩家最新信息，覆盖掉旧信息 */
-        redis.set(conf.redisCache.playerPrefix + req.session.userId,
+        /* 1，设置玩家最新信息，覆盖掉旧信息 */
+        redis.getset(conf.redisCache.playerPrefix + req.session.userId,
         JSON.stringify({
             id: req.session.userId,
             username: req.session.username,
@@ -23,8 +13,36 @@ module.exports = function(data ,wss, req){
             player_status: data.player_status,
             avatar_id : data.avatar_id
         }), 
-        function(err){
+        function(err, res){
             if (err) {return console.error('error redis response - ' + err)}
+             /* 2，检查该key是否存在，不存在则是新上线，否则是刷新信息 */
+            if(res === null){
+                wss.clients.forEach(function each(client) {
+                    if (client.readyState === WebSocket.OPEN && client.username !== req.session.username) {
+                        client.send(JSON.stringify({type: 'system', player_loc: 0 , text: '玩家 ' + data.nickname + ' 上线了'}))
+                    }
+                })
+            }
+            /* 对比新旧信息 */
+            else{
+                var oldPlayer = JSON.parse(res)
+                if(data.player_loc !== oldPlayer.player_loc){
+                    if(data.player_loc > 0){
+                        wss.clients.forEach(function each(client) {
+                            if (client.readyState === WebSocket.OPEN && client.userId !== req.session.userId) {
+                                client.send(JSON.stringify({type: 'system', player_loc: data.player_loc , text: '玩家 ' + data.nickname + ' 进入了房间'}));
+                            }
+                        })
+                    }
+                    else{
+                        wss.clients.forEach(function each(client) {
+                            if (client.readyState === WebSocket.OPEN && client.userId !== req.session.userId) {
+                                client.send(JSON.stringify({type: 'system', player_loc: oldPlayer.player_loc , text: '玩家 ' + data.nickname + ' 离开了房间'}));
+                            }
+                        })
+                    }
+                }
+            }
             /* 3，获取所有player玩家，发送广播 */
             redis.keys(conf.redisCache.playerPrefix + '*', function(err, list){
                 if (err) {return console.error('error redis response - ' + err)}
@@ -40,5 +58,4 @@ module.exports = function(data ,wss, req){
                 }
             })
         })
-    })
 }
