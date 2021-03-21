e144692505ff8e052eca8be1cbd325d8e39b1846
shurintou <shurintou@gmail.com>
2021-03-21 19:59:59 +0900

    增加加入房间逻辑

--- websocket/gameRoomListHandler.js
+++ websocket/gameRoomListHandler.js
@@ -3,9 +3,10 @@ const WebSocket = require('ws');
 const conf = require('../config/')
 
 module.exports = function(data ,wss, ws){
+    var allRooms = conf.redisCache.gameRoomPrefix + '*'
     /* id是0则直接获取现在的游戏房间列表 */
     if(data.id === 0){
-        redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+        redis.keys(allRooms, function(err, list){
             if (err) {return console.error('error redis response - ' + err)}
             if(list.length === 0){ 
                 ws.send(JSON.stringify({type: 'gameRoomList', data: [] }));
@@ -19,7 +20,7 @@ module.exports = function(data ,wss, ws){
     }
     /* id位NaN是新建的房间，需要分配一个id */
     else if(data.id === null || data.id === NaN){
-        redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+        redis.keys(allRooms, function(err, list){
             if (err) {return console.error('error redis response - ' + err)}
             var idOfList = []
             list.forEach( item => { idOfList.push(parseInt(item.split(conf.redisCache.gameRoomPrefix)[1]))})
@@ -58,18 +59,40 @@ module.exports = function(data ,wss, ws){
         })        
     }
     else if(data.id < 0){
+        let roomId = conf.redisCache.gameRoomPrefix + (-1 * data.id)
         /* 小于0，某玩家离开了房间 */
-        redis.get(conf.redisCache.gameRoomPrefix + (-1 * data.id), function(err, res){
+        redis.get(roomId, function(err, res){
             if (err) {return console.error('error redis response - ' + err)}
-            var room = JSON.parse(res)
-            /* 过滤掉离开的玩家 */
-            var newPlayerList = room.playerList.filter( player => player.id !==  ws.userId)
+            let room = JSON.parse(res)
+            let deleteId = room.playerList[data.seatIndex].id
+            let remainId = 0
+            /* 删掉离开的玩家 */
+            room.playerList[data.seatIndex] = {id: 0, cards: 0, ready: false}
+            let stillHasPlayer = false
+            for( let i = 0; i < Object.keys(room.playerList).length; i++){
+                if(room.playerList[i].id !== 0){
+                    stillHasPlayer = true
+                    remainId = room.playerList[i].id
+                    break
+                }
+            }
             /* 如果还有玩家在房间中则保留房间并广播 */
-            if(newPlayerList.length > 0){
-                room.playerList = newPlayerList
-                redis.set(conf.redisCache.gameRoomPrefix + (-1 * data.id), JSON.stringify(room), function(err){
+            if(stillHasPlayer){
+                /* 如果退出的是房主则换房主 */
+                if(room.owner === deleteId){
+                    room.owner = remainId
+                    redis.get(conf.redisCache.playerPrefix + remainId, function(err, res){
+                        if (err) {return console.error('error redis response - ' + err)}
+                        wss.clients.forEach(function each(client) {
+                            if (client.readyState === WebSocket.OPEN) {
+                                client.send(JSON.stringify({type: 'system', player_loc: (-1 * data.id) , text: '玩家 ' + JSON.parse(res).nickname + ' 成为了房主'}));
+                            }
+                        })
+                    })
+                }
+                redis.set(roomId, JSON.stringify(room), function(err){
                     if (err) {return console.error('error redis response - ' + err)}
-                    redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+                    redis.keys(allRooms, function(err, list){
                         if (err) {return console.error('error redis response - ' + err)}
                         if(list.length === 0){ 
                             wss.clients.forEach(function each(client) {
@@ -92,9 +115,9 @@ module.exports = function(data ,wss, ws){
             }
             /* 否则删除房间并广播 */
             else{
-                redis.del(conf.redisCache.gameRoomPrefix + (-1 * data.id), function(err){
+                redis.del(roomId, function(err){
                     if (err) {return console.error('error redis response - ' + err)}
-                    redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+                    redis.keys(allRooms, function(err, list){
                         if (err) {return console.error('error redis response - ' + err)}
                         if(list.length === 0){ 
                             wss.clients.forEach(function each(client) {
@@ -116,6 +139,51 @@ module.exports = function(data ,wss, ws){
                 })
             }
         })
+    }
+    /* 大于0，属于对某个房间的操作 */
+    else{
+        let roomId = conf.redisCache.gameRoomPrefix + data.id
+        /* 加入房间 */
+        if(data.action === 'enter'){
+            redis.get(roomId, function(err, res){
+                if (err) {return console.error('error redis response - ' + err)}
+                let room = JSON.parse(res)
+                let freeSeatIndex = -1
+                for( let i = 0; i < Object.keys(room.playerList).length; i++){
+                    if(room.playerList[i].id === 0 ){
+                        freeSeatIndex = i
+                        break
+                    }
+                }
+                if(freeSeatIndex === -1){
+                    ws.send(JSON.stringify({type: 'system', player_loc: 0 , text: '房间已满，无法进入'}))
+                    return
+                }
+                room.playerList[freeSeatIndex] = {id: ws.userId, cards: 0, ready: false}
+                redis.set(roomId, JSON.stringify(room), function(err){
+                    if (err) {return console.error('error redis response - ' + err)}
+                    redis.keys(allRooms, function(err, list){
+                        if (err) {return console.error('error redis response - ' + err)}
+                        if(list.length === 0){ 
+                            wss.clients.forEach(function each(client) {
+                                if (client.readyState === WebSocket.OPEN) {
+                                    client.send(JSON.stringify({type: 'gameRoomList', data: [] }));
+                                }
+                            })
+                            return
+                        }
+                        redis.mget(list, function(err, gameRoomList){
+                            if (err) {return console.error('error redis response - ' + err)}
+                            wss.clients.forEach(function each(client) {
+                                if (client.readyState === WebSocket.OPEN) {
+                                    client.send(JSON.stringify({type: 'gameRoomList', data: gameRoomList}));
+                                }
+                            });
+                        })
+                    })
+                })
+            })
+        }
     }
     
 }
