c4d09d9eca74d8193bc8d5b261e0068133e5e1b2
shurintou <shurintou@gamil.com>
2021-03-26 23:43:29 +0900

    不再使用store库来操作redis

--- services/loginService.js
+++ services/loginService.js
@@ -1,5 +1,4 @@
 const models = require('../common/models')
-const store = require('../common/session').store
 const redis = require('../database/redis')
 const conf = require('../config/')
 const errors = require('../common/errors')
@@ -36,45 +35,47 @@ module.exports = {
 function storeWrapper(req, account){
     return new Promise((resolve, reject) => {
         /* 通过验证能够登录，但须确认有没有重复session */
-        store.all(function(err, sessions){
-            if(err){
-                return reject({message: err})
-            }
-            else{
-               var hasLogin = false //是否有重复登录
-               var sessionId = ''  //已经登录的sessionID
-               for(var i = 0; i < sessions.length; i++){
-                    if(sessions[i].username === req.body.username){
-                        sessionId = conf.redisCache.sessionPrefix + sessions[i].sessionID
-                        hasLogin = true
-                        break
-                    }
-               }
-               if(hasLogin){
-                    /* 如果有重复登录，则获取已经登录session的ttl */
-                    redis.ttl(sessionId, function(err , res){
-                        if (err) {return console.error('error redis response - ' + err)}   
-                        /* 如果已经登录的session的ttl少于挤号判定时间，则删除该session，让另一边登录 */                         
-                        if(res < conf.ws.forceLogoutTtl){
-                            redis.del(sessionId, function(err){
-                                if (err) {return console.error('error redis response - ' + err)}                            
-                                return resolve({code: 200, message: '', account: {id: account.id, username: account.username, avatar_id: account.avatar_id, nickname: account.nickname }})
-                            })
+        redis.keys(conf.redisCache.sessionPrefix + '*', function(err, list){
+            if(err){ return reject({message: err})}
+            if(list.length === 0){ return resolve({code: 200, message: '', account: {id: account.id, username: account.username, avatar_id: account.avatar_id, nickname: account.nickname }}) }
+            redis.mget(list, function(err, res){
+                if(err){ return reject({message: err})}
+                let sessions = []
+                res.forEach( item => { sessions.push(JSON.parse(item)) })
+                var hasLogin = false //是否有重复登录
+                var sessionId = ''  //已经登录的sessionID
+                for(var i = 0; i < sessions.length; i++){
+                        if(sessions[i].username === req.body.username){
+                            sessionId = conf.redisCache.sessionPrefix + sessions[i].sessionID
+                            hasLogin = true
+                            break
                         }
-                        /* 如果大于挤号判定时间，则将该session的ttl设置为挤号判定时间 */
-                        else{
-                            redis.expire(sessionId , conf.ws.forceLogoutTtl, function(err){
-                                if (err) {return console.error('error redis response - ' + err)}                            
-                                return resolve(errors.DUBLICATE_ACCESS)
-                            })
-                        }
-                    }) 
-               }
-               /* 没有重复登录，则无须判断，直接登录成功 */
-               else{
-                 return resolve({code: 200, message: '', account: {id: account.id, username: account.username, avatar_id: account.avatar_id, nickname: account.nickname }})
-               }
-            }
+                }
+                if(hasLogin){
+                        /* 如果有重复登录，则获取已经登录session的ttl */
+                        redis.ttl(sessionId, function(err , res){
+                            if (err) {return console.error('error redis response - ' + err)}   
+                            /* 如果已经登录的session的ttl少于挤号判定时间，则删除该session，让另一边登录 */                         
+                            if(res < conf.ws.forceLogoutTtl){
+                                redis.del(sessionId, function(err){
+                                    if (err) {return console.error('error redis response - ' + err)}                            
+                                    return resolve({code: 200, message: '', account: {id: account.id, username: account.username, avatar_id: account.avatar_id, nickname: account.nickname }})
+                                })
+                            }
+                            /* 如果大于挤号判定时间，则将该session的ttl设置为挤号判定时间 */
+                            else{
+                                redis.expire(sessionId , conf.ws.forceLogoutTtl, function(err){
+                                    if (err) {return console.error('error redis response - ' + err)}                            
+                                    return resolve(errors.DUBLICATE_ACCESS)
+                                })
+                            }
+                        }) 
+                }
+                /* 没有重复登录，则无须判断，直接登录成功 */
+                else{
+                    return resolve({code: 200, message: '', account: {id: account.id, username: account.username, avatar_id: account.avatar_id, nickname: account.nickname }})
+                }
+            })
         })
     })
 }
