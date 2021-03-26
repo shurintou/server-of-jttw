03a5814e18d555f9bb55f80972a74bd2b42f5ad1
shurintou <shurintou@gmail.com>
2021-03-27 01:38:24 +0900

    增加新建房间，Redis房间列表keys长度为0时简单处理的逻辑

--- websocket/chatHandler.js
+++ websocket/chatHandler.js
@@ -3,15 +3,15 @@ const redis = require('../database/redis')
 const conf = require('../config/')
 
 module.exports =  function(data, wss, req){
-        wss.clients.forEach(function each(client) {
-            redis.get(conf.redisCache.playerPrefix + req.session.userId, function(err, res){
-                if (err) {return console.error('error redis response - ' + err)}
-                /* 与聊天信息的发送源玩家处于同一房间位置，或者就是发送源玩家本人的话，即可接收聊天信息 */
-                if ((data.player_loc === JSON.parse(res).player_loc || req.session.username === client.username ) && client.readyState === WebSocket.OPEN){
-                    client.send(JSON.stringify(data));
-                }
-            })
-        });
-    }
+    wss.clients.forEach(function each(client) {
+        redis.get(conf.redisCache.playerPrefix + req.session.userId, function(err, res){
+            if (err) {return console.error('error redis response - ' + err)}
+            /* 与聊天信息的发送源玩家处于同一房间位置，或者就是发送源玩家本人的话，即可接收聊天信息 */
+            if ((data.player_loc === JSON.parse(res).player_loc || req.session.username === client.username ) && client.readyState === WebSocket.OPEN){
+                client.send(JSON.stringify(data));
+            }
+        })
+    });
+}
 
   
