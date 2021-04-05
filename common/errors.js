bf3297b52f47194b7a656d1a591ed2ae67c0bb6a
shurintou <shurintou@gamil.com>
2021-04-05 18:07:47 +0900

    增加托管逻辑

--- common/errors.js
+++ common/errors.js
@@ -7,8 +7,9 @@ module.exports={
     DUBLICATE_ACCESS: {code: 409, message: '系统检测到重复登录，请稍后重试'},
     INVITATIONCODE_NOT_FOUND: {code: 406, message: '邀请码不存在，请重新输入'},
     INVITATIONCODE_USED: {code: 406, message: '邀请码已使用，请重新输入'},
-    ROOM_FULL:{code: 0, message: '房间已满员，无法进入'},
-    SEAT_FULL:{code: 0, message: '座位已被占，无法进入'},
-    ALREADY_IN_ROOM:{code: 0, message: '已在房间之中'},
-    POKER_TIMER_EXPIRED:{code: 0, message: '已超时，出牌失败'}
+    ROOM_FULL: {code: 0, message: '房间已满员，无法进入'},
+    SEAT_FULL: {code: 0, message: '座位已被占，无法进入'},
+    ALREADY_IN_ROOM: {code: 0, message: '已在房间之中'},
+    POKER_TIMER_EXPIRED: {code: 0, message: '已超时，出牌失败'},
+    SET_ONLINE_ERROR: {code: 0, message: '设置失败，请稍后重试'}
 }
