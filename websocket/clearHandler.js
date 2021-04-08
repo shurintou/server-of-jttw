99b6c5f7d73d653530b6c9767b536ca5a54b87cf
shurintou <shurintou@gamil.com>
2021-04-08 19:16:35 +0900

    修改wss close时逻辑，关闭时同时删除clearHandler中的计时器

--- websocket/clearHandler.js
+++ websocket/clearHandler.js
@@ -4,76 +4,103 @@ const logoutHandler = require('./logoutHandler')
 const WebSocket = require('ws')
 
 /* 定期清除失活的连接，session，player */  
-module.exports =  function(wss){setInterval(function checkConnections() {
-    wss.clients.forEach(function each(ws) {
-        if (ws.isAlive === false){
-            ws.terminate()
-            logoutHandler(wss, ws)
-        }
-        ws.isAlive = false;
-    }) 
-    redis.keys(conf.redisCache.sessionPrefix + '*', function(err, list){
-        if (err) {return console.error('error redis response - ' + err)}
-        if(list.length === 0){ 
-            /* 没有任何session存在了直接清空所有数据 */
-            redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
-                if (err) {return console.error('error redis response - ' + err)}
-                list.forEach( game => { redis.del(game) })
-            })
-            redis.keys(conf.redisCache.playerPrefix + '*', function(err, list){
-                if (err) {return console.error('error redis response - ' + err)}
-                list.forEach( player => { redis.del(player) })
-            })
-            return  
-        }
-        redis.mget(list, function(err, res){
+module.exports =  function(wss){
+    var clearHandlerTimer = setInterval(function checkConnections(){
+        wss.clients.forEach(function each(ws) {
+            if (ws.isAlive === false){
+                ws.terminate()
+                console.log('delete')
+                logoutHandler(wss, ws)
+            }
+            ws.isAlive = false;
+        }) 
+        redis.keys(conf.redisCache.sessionPrefix + '*', function(err, list){
             if (err) {return console.error('error redis response - ' + err)}
-            let sessions = []
-            res.forEach( item => { sessions.push(JSON.parse(item)) })
-            for(let i = 0; i < sessions.length; i++){
-                if(!sessions[i])continue
-                redis.ttl(conf.redisCache.sessionPrefix + sessions[i].sessionID, function(err, res){
+            if(list.length === 0){ 
+                /* 没有任何session存在了直接清空所有数据 */
+                redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
                     if (err) {return console.error('error redis response - ' + err)}
-                    if( res < conf.ws.deadTtl ){logoutHandler(wss, sessions[i])}
+                    list.forEach( game => { redis.del(game) })
                 })
-            }
-            let stillAlivePlayerIdList = []
-            for(let i = 0; i < sessions.length; i++){
-                if(!sessions[i])continue
-                stillAlivePlayerIdList.push( sessions[i].userId )
-            }
-            /* 清理房间 */
-            redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
-                if (err) {return console.error('error redis response - ' + err)}
-                if(list.length === 0){ 
-                    return 
-                }
-                redis.mget(list, function(err, gameRoomList){
+                redis.keys(conf.redisCache.playerPrefix + '*', function(err, list){
                     if (err) {return console.error('error redis response - ' + err)}
-                    gameRoomList.forEach( item => {
-                        let gameRoom = JSON.parse(item)
-                        if(gameRoom.status === 1){ return } //房间正在游戏中，不清理
-                        let stillHasPlayer = false
-                        /* 对房间每个位置进行检查 */
-                        for(let i = 0; i < Object.keys(gameRoom.playerList).length; i++){
-                            if(gameRoom.playerList[i].id !== 0){
-                                /* 该位置玩家还有session则还存在玩家 */
-                                if(stillAlivePlayerIdList.indexOf(gameRoom.playerList[i].id) !== -1){
-                                    stillHasPlayer = true
-                                }
-                                /* 该位置玩家没有session则把该位置清空 */
-                                else{
-                                    gameRoom.playerList[i] = {id: 0, cards: 0, win: 0, loss: 0, ready: false}
+                    list.forEach( player => { redis.del(player) })
+                })
+                return  
+            }
+            redis.mget(list, function(err, res){
+                if (err) {return console.error('error redis response - ' + err)}
+                let sessions = []
+                res.forEach( item => { sessions.push(JSON.parse(item)) })
+                for(let i = 0; i < sessions.length; i++){
+                    if(!sessions[i])continue
+                    redis.ttl(conf.redisCache.sessionPrefix + sessions[i].sessionID, function(err, res){
+                        if (err) {return console.error('error redis response - ' + err)}
+                        if( res < conf.ws.deadTtl ){logoutHandler(wss, sessions[i])}
+                    })
+                }
+                let stillAlivePlayerIdList = []
+                for(let i = 0; i < sessions.length; i++){
+                    if(!sessions[i])continue
+                    stillAlivePlayerIdList.push( sessions[i].userId )
+                }
+                /* 清理房间 */
+                redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+                    if (err) {return console.error('error redis response - ' + err)}
+                    if(list.length === 0){ 
+                        return 
+                    }
+                    redis.mget(list, function(err, gameRoomList){
+                        if (err) {return console.error('error redis response - ' + err)}
+                        gameRoomList.forEach( item => {
+                            let gameRoom = JSON.parse(item)
+                            if(gameRoom.status === 1){ return } //房间正在游戏中，不清理
+                            let stillHasPlayer = false
+                            /* 对房间每个位置进行检查 */
+                            for(let i = 0; i < Object.keys(gameRoom.playerList).length; i++){
+                                if(gameRoom.playerList[i].id !== 0){
+                                    /* 该位置玩家还有session则还存在玩家 */
+                                    if(stillAlivePlayerIdList.indexOf(gameRoom.playerList[i].id) !== -1){
+                                        stillHasPlayer = true
+                                    }
+                                    /* 该位置玩家没有session则把该位置清空 */
+                                    else{
+                                        gameRoom.playerList[i] = {id: 0, cards: 0, win: 0, loss: 0, ready: false}
+                                    }
                                 }
                             }
-                        }
-                        /* 房间还有玩家，则不删除房间 */
-                        if(stillHasPlayer){
-                            /* 如果房主不在房间了则换房主 */
-                            if(stillAlivePlayerIdList.indexOf(gameRoom.owner) === -1){
-                                gameRoom.owner = stillAlivePlayerIdList[0]
+                            /* 房间还有玩家，则不删除房间 */
+                            if(stillHasPlayer){
+                                /* 如果房主不在房间了则换房主 */
+                                if(stillAlivePlayerIdList.indexOf(gameRoom.owner) === -1){
+                                    gameRoom.owner = stillAlivePlayerIdList[0]
+                                }
+                                redis.set(conf.redisCache.gameRoomPrefix + gameRoom.id, JSON.stringify(gameRoom), function(err){
+                                    if (err) {return console.error('error redis response - ' + err)}
+                                    redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
+                                        if (err) {return console.error('error redis response - ' + err)}
+                                        if(list.length === 0){ 
+                                            wss.clients.forEach(function each(client) {
+                                                if (client.readyState === WebSocket.OPEN) {
+                                                    client.send(JSON.stringify({type: 'gameRoomList', data: [] }))
+                                                }
+                                            })
+                                            return
+                                        }
+                                        redis.mget(list, function(err, gameRoomList){
+                                            if (err) {return console.error('error redis response - ' + err)}
+                                            wss.clients.forEach(function each(client) {
+                                                if (client.readyState === WebSocket.OPEN) {
+                                                    client.send(JSON.stringify({type: 'gameRoomList', data: gameRoomList}))
+                                                }
+                                            })
+                                        })
+                                    })
+                                })
+                                return
                             }
-                            redis.set(conf.redisCache.gameRoomPrefix + gameRoom.id, JSON.stringify(gameRoom), function(err){
+                            /* 否则删除房间 */
+                            redis.del(conf.redisCache.gameRoomPrefix + gameRoom.id, function(err){
                                 if (err) {return console.error('error redis response - ' + err)}
                                 redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
                                     if (err) {return console.error('error redis response - ' + err)}
@@ -95,34 +122,12 @@ module.exports =  function(wss){setInterval(function checkConnections() {
                                     })
                                 })
                             })
-                            return
-                        }
-                        /* 否则删除房间 */
-                        redis.del(conf.redisCache.gameRoomPrefix + gameRoom.id, function(err){
-                            if (err) {return console.error('error redis response - ' + err)}
-                            redis.keys(conf.redisCache.gameRoomPrefix + '*', function(err, list){
-                                if (err) {return console.error('error redis response - ' + err)}
-                                if(list.length === 0){ 
-                                    wss.clients.forEach(function each(client) {
-                                        if (client.readyState === WebSocket.OPEN) {
-                                            client.send(JSON.stringify({type: 'gameRoomList', data: [] }))
-                                        }
-                                    })
-                                    return
-                                }
-                                redis.mget(list, function(err, gameRoomList){
-                                    if (err) {return console.error('error redis response - ' + err)}
-                                    wss.clients.forEach(function each(client) {
-                                        if (client.readyState === WebSocket.OPEN) {
-                                            client.send(JSON.stringify({type: 'gameRoomList', data: gameRoomList}))
-                                        }
-                                    })
-                                })
-                            })
                         })
                     })
                 })
             })
-        })
-    }) 
-}, conf.ws.checkPeriod)}
+        }) 
+    }, conf.ws.checkPeriod)
+    //将计时器绑定到wss上，wss关闭时删除计时器
+    wss.clearHandlerTimerId = clearHandlerTimer[Symbol.toPrimitive]()
+}
