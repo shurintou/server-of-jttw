42161c8408f49ac1df1bf31d69fa87f6838c84a0
shurintou <shurintou@gamil.com>
2021-04-13 01:42:16 +0900

    增加查看战绩时返回某玩家的游戏战绩列表逻辑

--- routers/inforSearch.js
+++ routers/inforSearch.js
@@ -13,5 +13,15 @@ router.get('/player/record/:id', function (req, res) {
     })
 })
 
+router.get('/game/records/:page', function(req, res){
+    infoSearchService.getGameRecordsList(req)
+    .then(result => {
+        res.status(200).json({code: result.code,  message: result.message, pageNum: result.pageNum ? result.pageNum : 0, list: result.list? result.list : []})
+    })
+    .catch(err => {
+        res.status(err.code? err.code:500).json({message: err.message})
+    })
+})
+
 
 module.exports = router
