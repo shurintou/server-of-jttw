58297ae6d19de6795e8968fae261421c8c31db67
shurintou <shurintou@gmail.com>
2023-09-21 01:09:29 +0900

    chore: 为路由拦截器增添类型注释。

--- common/routerInterceptor.js
+++ common/routerInterceptor.js
@@ -2,24 +2,36 @@
 
 const conf = require('../config/')
 const errors = require('./errors')
+/** 
+ * @typedef {import('../types/http').ClientRequest}
+ * @typedef {import('../types/http').ClientResponse}
+ * @typedef {import('express').NextFunction}
+ */
 
-module.exports = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', conf.frontOrigin)
-    res.header('Access-Control-Allow-Headers', conf.httpHeaders.allowHeaders)
-    res.header('Access-Control-Allow-Methods', conf.httpHeaders.allowMethods)
-    res.header('Access-Control-Allow-Credentials', conf.httpHeaders.allowCredentials)
-    //如果请求的路径是登录或注册，则通过
-    if (req._parsedUrl.path.indexOf('/login') !== -1 || req._parsedUrl.path.indexOf('/register') !== -1) {
-        next()
-    }
-    else {
-        //如果请求的是其他路径，且请求中带有session.username，则通过
-        if (req.session.username) {
+module.exports =
+    /** 
+     * @param {ClientRequest} req
+     * @param {ClientResponse} res
+     * @param {NextFunction} next
+     * @returns {void}
+     */
+    function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', conf.frontOrigin)
+        res.header('Access-Control-Allow-Headers', conf.httpHeaders.allowHeaders)
+        res.header('Access-Control-Allow-Methods', conf.httpHeaders.allowMethods)
+        res.header('Access-Control-Allow-Credentials', conf.httpHeaders.allowCredentials)
+        //如果请求的路径是登录或注册，则通过
+        if (req._parsedUrl.path.indexOf('/login') !== -1 || req._parsedUrl.path.indexOf('/register') !== -1) {
             next()
         }
-        //否则报session过期错误
         else {
-            res.status(200).json(errors.SESSION_TIMEOUT)
+            //如果请求的是其他路径，且请求中带有session.username，则通过
+            if (req.session.username) {
+                next()
+            }
+            //否则报session过期错误
+            else {
+                res.status(200).json(errors.SESSION_TIMEOUT)
+            }
         }
     }
-}
