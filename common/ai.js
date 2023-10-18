c515bca83fd45bcfe1092b0cbd64ab363e4bf2f5
shurintou <shurintou@gmail.com>
2023-10-19 00:36:47 +0900

    feature: 新增playCardFilter过滤器以统一牌型。

--- common/ai.js
+++ common/ai.js
@@ -50,13 +50,27 @@ function deDuplicatedCombination(allCombination) {
 
 }
 
+
+/** 
+ * @summary 过滤出排列组合中可打出的统一牌型。
+ * @param {number[]} combination 排列组合。
+ * @returns {boolean} 是否是可打出的统一牌型结果。
+ */
+function playCardFilter(combination) {
+    if (combination.every(card => poker.getIndexOfCardList(card).num === 100)) {
+        return true // 所有牌为反弹牌的情况返回TRUE
+    }
+    // 过滤出所有下标为1以上的牌的牌面等于基本牌的牌面或等于变身牌的牌型
+    return combination.every((card, index) => index === 0 || (poker.getIndexOfCardList(card).num === poker.getIndexOfCardList(combination[0]).num || card > 100))
+}
+
 /** 
  * @summary 获取指定张数时玩家手中牌能打出的所有排列组合(仅去重和统一牌型，但不考虑牌型是否可管上现在牌池中的牌型)。
  * @description 只要定下了第一张基本牌，后面的牌均为附属牌，
  * 所以对于手中5张牌来说，取下标0,1,2,3,4分别作为基本牌，剩下的4张则作为附属牌来进行组合即可。
  * 所以若现有牌池为2张，玩家手中牌为5张，则所有可打出的组合数为5*C4/2 = 30
  * @param {number[]} allCards 玩家手中所有牌
- * @param {number} [count = 1] 需要打出的指定张数(默认为1)
+ * @param {number} [count = 1] 需要打出的指定张数(小于等于玩家玩家手中所有牌张数，默认为1)
  * @returns {number[][]} 所有可打出的牌组合的结果(仅去重和统一牌型，但不考虑牌型是否可管上现在牌池中的牌型)
  */
 function getPlayCardsListBySpecifiedCount(allCards, count = 1) {
@@ -88,7 +102,9 @@ function getPlayCardsListBySpecifiedCount(allCards, count = 1) {
     /** @type {number[][]} 玩家手中牌能打出的所有排列组合(去重) */
     const deDuplicatedResult = [] = deDuplicatedCombination(result)
 
-    return deDuplicatedResult
+    const playCardSListResult = deDuplicatedResult.filter(playCardFilter)
+
+    return playCardSListResult
 
 }
 
