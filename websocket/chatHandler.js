c66bd8e3565ad3e8205e13b59f9de45c54008e95
shurintou <shurintou@gmail.com>
2023-11-01 23:54:05 +0900

    performance: 调整代码，避免不必要的多次调用同一方法。

--- websocket/chatHandler.js
+++ websocket/chatHandler.js
@@ -17,22 +17,22 @@ const logger = require('../common/log')
  */
 module.exports = function (data, wss, req) {
     try {
-        wss.clients.forEach(function each(client) {
-            redis.get(conf.redisCache.playerPrefix + req.session.userId, function (err, res) {
-                if (err) { return logger.error('error redis response - ' + err) }
-                if (res === null) { return logger.error(conf.redisCache.playerPrefix + req.session.userId + errors.CACHE_DOES_NOT_EXIST) }
-                try {
-                    /** @type {RedisCachePlayer} */
-                    const player = JSON.parse(res)
+        redis.get(conf.redisCache.playerPrefix + req.session.userId, function (err, res) {
+            if (err) { return logger.error('error redis response - ' + err) }
+            if (res === null) { return logger.error(conf.redisCache.playerPrefix + req.session.userId + errors.CACHE_DOES_NOT_EXIST) }
+            try {
+                /** @type {RedisCachePlayer} */
+                const player = JSON.parse(res)
+                wss.clients.forEach(function each(client) {
                     /* 与聊天信息的发送源玩家处于同一房间位置，或者就是发送源玩家本人的话，即可接收聊天信息 */
                     if ((data.player_loc === player.player_loc || req.session.username === client.username) && client.readyState === WebSocket.OPEN) {
                         client.send(JSON.stringify(data))
                     }
-                }
-                catch (e) {
-                    logger.error(e)
-                }
-            })
+                })
+            }
+            catch (e) {
+                logger.error(e)
+            }
         })
     }
     catch (e) {
